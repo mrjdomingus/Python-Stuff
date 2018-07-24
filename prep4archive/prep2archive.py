f9dd839116dfea1a42a810bb5a6501acfef86ca2
Marcel Domingus <domingus@xs4all.nl>
2018-07-24 14:18:22 +0200

    Some minor code cleanups

--- prep4archive/prep2archive.py
+++ prep4archive/prep2archive.py
@@ -1,8 +1,9 @@
 # Also see https://pymotw.com/2/zipfile/ for how to use zipfile module
 
-import logging, sys
+import logging
+import sys
 import os
-from os import listdir, mkdir, chdir, rename, remove  
+from os import listdir, mkdir, chdir, rename, remove
 os.environ["UNRAR_LIB_PATH"] = "/usr/lib/libunrar.so"
 from unrar import rarfile
 
@@ -22,17 +23,16 @@ import shutil
 from titlecase import titlecase
 from tempfile import mkdtemp
 
-from collections import namedtuple  
+from collections import namedtuple
 from itertools import groupby
 
 
 class FileBase:
     # Order of extensions to keep in descending prererence
-    ext_order = {'.zip': 0, '.pdf': 10, '.epub': 20, '.azw3': 30, '.mobi': 40 }
-    
-    def __init__(self, filename):
-        self.filename = filename # We expect the full filename here.
+    ext_order = {'.zip': 0, '.pdf': 10, '.epub': 20, '.azw3': 30, '.mobi': 40}
 
+    def __init__(self, filename):
+        self.filename = filename  # We expect the full filename here.
 
     def process_file(self):
         pass
@@ -41,12 +41,13 @@ class FileBase:
         bn = basename(self.filename)
         (bn, ext) = splitext(bn)
         (head, _) = split(self.filename)
-        m = re.search(r"\W(\d+)(nd|rd|th)$",bn)
+        m = re.search(r"\W(\d+)(nd|rd|th)$", bn)
         if m:
             logging.debug("Match edition: {0}".format(m.string))
             (start, end) = m.span()
             length = (end - start)
-            bn = bn[:start] + ", " + m.group(1) + m.group(2) + " Edition" + bn[start+length:]
+            bn = bn[:start] + ", " + \
+                m.group(1) + m.group(2) + " Edition" + bn[start+length:]
 
         # Kludge to deal with zip files that have been processed earlier
         if True:
@@ -54,7 +55,7 @@ class FileBase:
         else:
             bn = titlecase(bn)
 
-        self.filename = join(head,(bn + ext))
+        self.filename = join(head, (bn + ext))
         logging.debug("New filename: {0}".format(self.filename))
         return(self.filename)
 
@@ -69,53 +70,57 @@ class FileBase:
             # Split each file name in a (root, ext) tuple
             for t in [(splitext(doc)) for doc in namelist]:
                 # Convert tuple in dict (via intermediate list) and convert then into namedtuple of type Document
-                documents.append(Document(**(dict(zip(['basename', 'ext'],list(t))))))
-        except Exception as e: print(e)
+                documents.append(
+                    Document(**(dict(zip(['basename', 'ext'], list(t))))))
+        except Exception as e:
+            print(e)
 
-        keyfunc = lambda doc: doc.basename
+        def keyfunc(doc): return doc.basename
 
         documents = sorted(documents, key=keyfunc)
 
-        doc_map = {}  
+        doc_map = {}
         # Use dict comprehensions to group all extensions by root part of file name
         try:
             doc_map = {
-                key: [doc.ext for doc in group] 
+                key: [doc.ext for doc in group]
                 for key, group in groupby(documents, lambda doc: doc.basename)
             }
-        except Exception as e: print(e)
+        except Exception as e:
+            print(e)
 
         for key in doc_map.keys():
             if len(doc_map[key]) == 1:
-                no_more_dups.append(key + doc_map[key][0] )
+                no_more_dups.append(key + doc_map[key][0])
             else:
-                extfunc = lambda ext: FileBase.ext_order.get(ext, 999)
+                def extfunc(ext): return FileBase.ext_order.get(ext, 999)
                 extensions = sorted(doc_map[key], key=extfunc)
                 for ext in extensions:
                     if ext == '.zip':
-                        no_more_dups.append(key + ext )
+                        no_more_dups.append(key + ext)
                     else:
-                        no_more_dups.append(key + ext )
+                        no_more_dups.append(key + ext)
                         break
-        
+
         return no_more_dups
+
 
 class RAR_or_ZIPFile(FileBase):
     def __init__(self, filename, out_dir):
         super().__init__(filename)
-        (_, ext) =splitext(self.filename)
+        (_, ext) = splitext(self.filename)
         if ext.lower() == ".rar":
             self.__rar_or_zip = rarfile.RarFile(filename)
         elif ext.lower() == ".zip":
             self.__rar_or_zip = zipfile.ZipFile(filename)
         else:
-            logging.error("Unsupported type of compressed file: {0}!".format(ext))
+            logging.error(
+                "Unsupported type of compressed file: {0}!".format(ext))
         self.__out_dir = out_dir
-
 
     def process_file(self):
         super().process_file()
-        
+
         TMPDIR = mkdtemp()
         chdir(TMPDIR)
 
@@ -123,24 +128,27 @@ class RAR_or_ZIPFile(FileBase):
 
         namelist = FileBase.unduplicate(namelist)
 
-        # First empty TMPDIR        
-        files_to_clean = [f for f in listdir(TMPDIR) if isfile(join(TMPDIR, f))]
+        # First empty TMPDIR
+        files_to_clean = [f for f in listdir(
+            TMPDIR) if isfile(join(TMPDIR, f))]
         for f in files_to_clean:
-            remove(join(TMPDIR,f))
+            remove(join(TMPDIR, f))
 
         # Extract files from unduplicated list
         for name in namelist:
             self.__rar_or_zip.extract(name, TMPDIR)
-        
-        extracted_files = [f for f in listdir(TMPDIR) if isfile(join(TMPDIR, f))]
+
+        extracted_files = [f for f in listdir(
+            TMPDIR) if isfile(join(TMPDIR, f))]
 
         # Below code assumes that temporary, extracted files are located in current directory
         for tmpfile in extracted_files:
             # Improve file name
-            rename(tmpfile, FileBase(join(TMPDIR,tmpfile)).rename())
+            rename(tmpfile, FileBase(join(TMPDIR, tmpfile)).rename())
 
         # Reacquire list of files
-        extracted_files = [f for f in listdir(TMPDIR) if isfile(join(TMPDIR, f))]
+        extracted_files = [f for f in listdir(
+            TMPDIR) if isfile(join(TMPDIR, f))]
 
         if len(extracted_files) == 0:
             # Do nothing
@@ -152,9 +160,11 @@ class RAR_or_ZIPFile(FileBase):
             # Create zip file
             # Kludge to deal with zip files that have been processed earlier
             if False:
-                zipname = join(self.__out_dir,  titlecase(splitext(basename(self.filename))[0].replace("-", " ")) + ".zip")
+                zipname = join(self.__out_dir,  titlecase(
+                    splitext(basename(self.filename))[0].replace("-", " ")) + ".zip")
             else:
-                zipname = join(self.__out_dir,  titlecase(splitext(basename(self.filename))[0]) + ".zip")
+                zipname = join(self.__out_dir,  titlecase(
+                    splitext(basename(self.filename))[0]) + ".zip")
             try:
                 with ZipFile(zipname, 'w') as myzip:
                     try:
@@ -163,14 +173,16 @@ class RAR_or_ZIPFile(FileBase):
                     finally:
                         myzip.close()
                         rename(zipname, FileBase(zipname).rename())
-            except Exception as e: print(e)
+            except Exception as e:
+                print(e)
 
         # Delete temp directory
         shutil.rmtree(TMPDIR)
 
 
 def main(in_dir, out_dir):
-    logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+    logging.basicConfig(format='%(levelname)s:%(message)s',
+                        level=logging.DEBUG)
 
     chdir(in_dir)
 
@@ -185,9 +197,9 @@ def main(in_dir, out_dir):
         logging.info("Processing: {0}".format(filename))
         rar = RAR_or_ZIPFile(join(in_dir, filename), out_dir)
         rar.process_file()
-    
+
     print("Done!")
 
-if __name__ == '__main__':
-   main("/mnt/hgfs/DDD", "/mnt/hgfs/DDD/output")
 
+if __name__ == '__main__':
+    main("/mnt/hgfs/DDD", "/mnt/hgfs/DDD/output")
