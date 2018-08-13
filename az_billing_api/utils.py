a6049a40d9c8dfcd34c6a0774b6b2767ce282361
Marcel Domingus <domingus@xs4all.nl>
2018-08-13 21:12:32 +0200

    Fix bug in calc_amount and add some test code

--- az_billing_api/utils.py
+++ az_billing_api/utils.py
@@ -28,10 +28,58 @@ def calc_rates_dict(dict):
     return df.to_dict()
 
 def calc_amount(dict, qty):
+    # Convert dict to dataframe
+    df = pd.DataFrame.from_dict(dict)
     # Only consider rate intervals with a lower_limit below the actually used quantity
-    mask = dict['lower_limit'] < qty
+    mask = df['lower_limit'] <= qty
     # Pick the last interval
-    interval  = dict[mask].iloc[-1]
+    interval  = df[mask].iloc[-1]
     # Calculate value as cumulative value of previous interval plus the consumption value in the present interval
     value = interval['price'] * (qty - interval['lower_limit'] ) + interval['cum_value']
     return value
+
+if __name__ == "__main__":
+    import json
+    s = '''
+            {
+        "lower_limit": {
+            "0": 0.0,
+            "1": 10235.0,
+            "2": 51195.0,
+            "3": 153595.0,
+            "4": 511995.0
+        },
+        "price": {
+            "0": 0.0733671,
+            "1": 0.0699939,
+            "2": 0.059031,
+            "3": 0.042165,
+            "4": 0.042165
+        },
+        "upper_limit": {
+            "0": 10235.0,
+            "1": 51195.0,
+            "2": 153595.0,
+            "3": 511995.0,
+            "4": 1.7976931348623157e308
+        },
+        "interval_value": {
+            "0": 750.9122685000001,
+            "1": 2866.950144,
+            "2": 6044.7744,
+            "3": 15111.936,
+            "4": 7.579973103146954e306
+        },
+        "cum_value": {
+            "0": 0.0,
+            "1": 750.9122685000001,
+            "2": 3617.8624125,
+            "3": 9662.6368125,
+            "4": 24774.572812500002
+        }
+        }
+        '''
+    dict = json.loads(s)
+    print("Result of calc_amount: ", calc_amount(dict, 0))
+
+
